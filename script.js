3600825c623c5fc2f11eaa58958126af21ca49b8
stevewhitmore <s25w89@gmail.com>
2021-02-23 21:33:06 -0600

    Added linting and a pipeline

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const thunderAudio = document.querySelector('#storm-audio');
-
-function playSound() {
-    thunderAudio.volume = 1;
-    thunderAudio.play();
-    toggleVisible();
-}
-
-function pauseSound() {
-  thunderAudio.pause();
-  toggleVisible();
-}
+const thunderAudioEl = document.querySelector('#storm');
+const playButtonEl = document.querySelector('#play');
+const pauseButtonEl = document.querySelector('#pause');
 
 function toggleVisible() {
   const buttons = document.querySelectorAll('button');
 
   buttons.forEach(b => {
     b.classList.toggle('visible');
-  })
+  });
 }
+
+function playSound() {
+  thunderAudioEl.volume = 1;
+  thunderAudioEl.play();
+  toggleVisible();
+}
+
+function pauseSound() {
+  thunderAudioEl.pause();
+  toggleVisible();
+}
+
+playButtonEl.addEventListener('click', () => {
+  playSound();
+});
+
+pauseButtonEl.addEventListener('click', () => {
+  pauseSound();
+});
